0f9a0254fc6599d79c081878c7f77fb12e56a0ac
Priyank raghwani <priyankraghwani98@gmail.com>
2020-06-05 15:32:26 +0530

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnitTestingDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Message = "You are in Index Page";
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            ViewBag.Message = "You are in Contactus Page";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "You are in About us page";
+            return View();
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnitTestingDemo.Models;
+
+namespace UnitTestingDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [NonAction]
+        public List<Employee> GetEmployeeList()
+        {
+            return new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Priyank",
+                    BirthDate = DateTime.Now,
+                    Age = 22
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Parth",
+                    BirthDate = DateTime.Now,
+                    Age = 22
+                },
+                new Employee
+                {
+                    Id = 3,
+                    Name = "Mayur",
+                    BirthDate = DateTime.Now,
+                    Age = 21
+                },
+                new Employee
+                {
+                    Id = 4,
+                    Name = "Swati",
+                    BirthDate = DateTime.Now,
+                    Age = 24
+                },
+            };
+        }
+
+        [HttpGet]
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetEmployees()
+        {
+            List<Employee> employees = GetEmployeeList();
+            return View(employees);
+        }
+    }
+}
